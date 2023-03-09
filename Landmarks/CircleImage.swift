d5cba135160d4d7a07868a90be82007c42ed30fc
Stefan Baggoo <itsrhyett@gmail.com>
2023-03-09 14:55:09 -0400

    Learning Swift up

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CirlceImage.swift
+//  Landmarks
+//
+//  Created by Stefan Baggoo on 3/9/23.
+//
+
+import SwiftUI
+
+struct CirlceImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CirlceImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CirlceImage()
+    }
+}
