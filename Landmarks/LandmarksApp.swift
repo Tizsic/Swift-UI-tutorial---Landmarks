e4d567f933a970fa9123957e41492bc89b7ed631
Stefan Baggoo <itsrhyett@gmail.com>
2023-03-09 14:21:26 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Stefan Baggoo on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
